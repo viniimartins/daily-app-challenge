11752882d3b86fbc91bb492e947e0ecd1f12b94d
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2024-10-18 11:06:23 -0300

    chore: eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import fastify from "fastify";
-
+import fastify from 'fastify'
 
 const app = fastify()
 
-
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('HTTP Server Running! 🚀')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running! 🚀')
+  })
