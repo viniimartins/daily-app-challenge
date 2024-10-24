d451ce94db90174f1757087fccb49958e3eb9c7d
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2024-10-23 22:39:56 -0300

    refactor: zod envs

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,6 +11,7 @@ const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
   console.error('⚠️ Invalid environment variables', _env.error.format())
+
   throw new Error('Invalid environment variables.')
 }
 
