85a50e0e9f7645929711e0a139edf9aab9debd5b
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2024-10-23 22:18:06 -0300

    feat: envs

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const tables = await knex('users').select('*')
 
   return tables
 })
